1288dea4d7601d7e0f5478072b76e8d54ce26cfd
OthibengKgalaletso <othibengkgalaletso3@gmail.com>
2023-02-22 05:57:26 -0800

    6- update abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * This function outputs the absolute value of integer
+ * @r: integer to check
+ * Return: the absolute value of integer r
+ */
+
+
+int _abs(int r)
+{
+	if (r >= 0)
+	return (r);
+	else
+		return (r * -1);
+}
