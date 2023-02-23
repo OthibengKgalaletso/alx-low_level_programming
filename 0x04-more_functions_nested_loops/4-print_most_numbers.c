c5b8532e10c05534594077979ed702162cacd959
OthibengKgalaletso <othibengkgalaletso3@gmail.com>
2023-02-23 14:23:58 -0800

    Task 4 fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 /**
  * print_most_numbers - prints numbers from 0 to 9
  * but not 2 and 4
- * Return: void
  */
 
 void print_most_numbers(void)
@@ -11,7 +10,7 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == 2 || c == 4))
+		if (!(c == 2 && c == 4))
 		_putchar(c);
 	}
 	_putchar('\n');
