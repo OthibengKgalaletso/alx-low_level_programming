1c132b4ed698041352e836f099e27d10d5b307c6
OthibengKgalaletso <othibengkgalaletso3@gmail.com>
2023-02-24 11:41:30 -0800

    101- fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,43 +2,22 @@
 
 /**
  * print_number - this function prints an integer.
- * @n: integer type number
+ * @n: number to print
  *
  */
+
 void print_number(int n)
 {
-	long m;
-	int c;
-	long num;
+	unsigned int num = n;
 
-	num = n;
-	if (num < 0)
+	if (n < 0)
 	{
-		num *= -1;
 		_putchar('-');
+		num = -num;
 	}
 
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
+	if (num > 9)
+		print_number(num / 10);
 
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+	_putchar(num % 10 + '0');
 }
