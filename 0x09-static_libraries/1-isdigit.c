53c1d4ec6e719e8fab6907c5aad41f0586c77b4b
OthibengKgalaletso <othibengkgalaletso3@gmail.com>
2023-03-10 09:36:03 -0800

    0x09 static libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+* _isdigit - checks for a digit (0 through 9)
+* @num: int to be checked
+* Return: 1 if num is a digit 
+* or 0 if otherwise.
+*
+*/
+
+
+int _isdigit(int num)
+
+
+{
+
+return (num >= '0' && num <= '9');
+
+
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _memcpy - function that copies a memory area
+ * @dest: buffer to copy to
+ * @src: what is being copied
+ * @n: n bytes of @src
+ *
+ * Return: Always 0 (Success)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
